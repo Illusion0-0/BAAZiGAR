80f041777f7f5bd89d61c79be9075cfa081e73f8
hmrserver <hmrserver@gmail.com>
2022-11-03 12:23:24 +0530

    Added the cms functions

--- cred.cpp
+++ cred.cpp
@@ -1,57 +1,98 @@
 #include <bits/stdc++.h>
+
+#include "cms.cpp"
 #include "cms.h"
 using namespace std;
 
-int main(){
-
-    bool ok=true;
-    do{
+int main() {
+    bool ok = true;
+    ContactManager cms;
+    do {
         int operation;
-        cout<<"-----CONTACT MANAGER-----"<<endl;
-        cout<<"-----CHOOSE OPERATION TO PERFORM-----"<<endl;
-        cout<<"-----1) ADD NEW CONTACT-----"<<endl;
-        cout<<"-----2) SEARCH BY FIRST NAME-----"<<endl;
-        cout<<"-----3) SEARCH BY LAST NAME-----"<<endl;
-        cout<<"-----4) SEARCH BY PHONE NUMBER-----"<<endl;
-        cout<<"-----5) PARTIAL SEARCH BY FIRST NAME-----"<<endl;
-        cout<<"-----6) PARTIAL SEARCH BY LAST NAME-----"<<endl;
-        cout<<"-----7) PARTIAL SEARCH BY PHONE NUMBER-----"<<endl;
-        cout<<"-----5) EXIT-----"<<endl;
-        cin>>operation;
-        switch(operation){
+
+        string firstName, lastName, phoneNumber, firstNamePrefix,
+            lastNamePrefix, phoneNumberPrefix;
+        vector<Contact> result;
+        cout << "-----CONTACT MANAGER-----" << endl;
+        cout << "-----CHOOSE OPERATION TO PERFORM-----" << endl;
+        cout << "-----1) ADD NEW CONTACT-----" << endl;
+        cout << "-----2) SEARCH BY FIRST NAME-----" << endl;
+        cout << "-----3) SEARCH BY LAST NAME-----" << endl;
+        cout << "-----4) SEARCH BY PHONE NUMBER-----" << endl;
+        cout << "-----5) PARTIAL SEARCH BY FIRST NAME-----" << endl;
+        cout << "-----6) PARTIAL SEARCH BY LAST NAME-----" << endl;
+        cout << "-----7) PARTIAL SEARCH BY PHONE NUMBER-----" << endl;
+        cout << "-----8) EXIT-----" << endl;
+        cin >> operation;
+        switch (operation) {
             case 1:
-                cms.addContact();
+
+                cms.getContact();
                 break;
 
             case 2:
-                cms.searchByFirstName();
+                cout << "Enter first name to search: ";
+                cin >> firstName;
+                result = cms.searchByFirstName(firstName);
+                for (auto record : result) {
+                    cout << record.getFirstName() << " " << record.getLastName()
+                         << " " << record.getPhoneNumber() << endl;
+                }
                 break;
 
             case 3:
-                cms.searchByLirstName();
+                cout << "Enter last name to search: ";
+                cin >> lastName;
+                result = cms.searchByLastName(lastName);
+                for (auto record : result) {
+                    cout << record.getFirstName() << " " << record.getLastName()
+                         << " " << record.getPhoneNumber() << endl;
+                }
                 break;
-
             case 4:
-                cms.searchByPhoneNumber();
+                cout << "Enter Phone Number to search: ";
+                cin >> phoneNumber;
+                result = cms.searchByPhoneNumber(phoneNumber);
+                for (auto record : result) {
+                    cout << record.getFirstName() << " " << record.getLastName()
+                         << " " << record.getPhoneNumber() << endl;
+                }
                 break;
-
             case 5:
-                cms.searchByFirstNamePrefix();
+                cout << "Enter first name prefix to search: ";
+                cin >> firstNamePrefix;
+                result = cms.searchByFirstNamePrefix(firstNamePrefix);
+                for (auto record : result) {
+                    cout << record.getFirstName() << " " << record.getLastName()
+                         << " " << record.getPhoneNumber() << endl;
+                }
                 break;
 
             case 6:
-                cms.searchByLirstNamePrefix();
+                cout << "Enter last name prefix to search: ";
+                cin >> lastNamePrefix;
+                result = cms.searchByLastNamePrefix(lastNamePrefix);
+                for (auto record : result) {
+                    cout << record.getFirstName() << " " << record.getLastName()
+                         << " " << record.getPhoneNumber() << endl;
+                }
                 break;
-
             case 7:
-                cms.searchByPhoneNumberPrefix();
+                cout << "Enter phone number prefix to search: ";
+                cin >> phoneNumberPrefix;
+                result = cms.searchByPhoneNumberPrefix(phoneNumberPrefix);
+                for (auto record : result) {
+                    cout << record.getFirstName() << " " << record.getLastName()
+                         << " " << record.getPhoneNumber() << endl;
+                }
+                break;
+            case 8:
+                ok = false;
                 break;
 
             default:
-                cout<<"Please Choose between 1 to 7"<<endl;
-
+                cout << "Please Choose between 1 to 8" << endl;
         }
 
-    }
-    while(ok);
+    } while (ok);
 }
