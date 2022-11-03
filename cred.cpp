06030b28fa8dd246c7aece08b5a35c38cb39debd
Prince Harshwardhan Kapare <76548962+Harshwardhan4k@users.noreply.github.com>
2022-11-03 11:55:44 +0530

    Update cred.cpp

--- cred.cpp
+++ cred.cpp
@@ -1,3 +1,60 @@
-//
-// Created by test on 03-11-2022.
-//
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    bool ok=true;
+
+    do{
+        int operation;
+        cout<<"-----CONTACT MANAGER-----"<<endl;
+        cout<<"-----CHOOSE OPERATION TO PERFORM-----"<<endl;
+        cout<<"-----1) ADD NEW CONTACT-----"<<endl;
+        cout<<"-----2) SEARCH BY FIRST NAME-----"<<endl;
+        cout<<"-----3) SEARCH BY LAST NAME-----"<<endl;
+        cout<<"-----4) SEARCH BY PHONE NUMBER-----"<<endl;
+        cout<<"-----5) PARTIAL SEARCH BY FIRST NAME-----"<<endl;
+        cout<<"-----6) PARTIAL SEARCH BY LAST NAME-----"<<endl;
+        cout<<"-----7) PARTIAL SEARCH BY PHONE NUMBER-----"<<endl;
+
+
+
+        cout<<"-----5) EXIT-----"<<endl;
+        cin>>operation;
+        switcih(operation){
+            case 1:
+                cms.addContact();
+                break;
+
+            case 2:
+                cms.searchByFirstName();
+                break;
+
+            case 3:
+                cms.searchByLirstName();
+                break;
+
+            case 4:
+                cms.searchByPhoneNumber();
+                break;
+
+            case 5:
+                cms.searchByFirstNamePrefix();
+            break;
+
+            case 6:
+                cms.searchByLirstNamePrefix();
+            break;
+
+            case 7:
+                cms.searchByPhoneNumberPrefix();
+            break;
+
+            default:
+                cout<<"Please Choose between 1 to 7"<<endl;
+
+        }
+
+    }
+    while(ok);
+
+}
