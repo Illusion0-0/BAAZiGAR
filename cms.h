e8b17ac8b7c0cc4fe582d74d44a674606dafcb82
hmrserver <hmrserver@gmail.com>
2022-11-03 11:35:02 +0530

    Basic Structure of header file

--- cms.h
+++ cms.h
@@ -1,8 +1,40 @@
-//
-// Created by test on 03-11-2022.
-//
+// Problem Statement: Build a simple contact management system where contact
+// contains first name, last name, phone number Created by test on 03-11-2022.
+// Author: Prince Patel, Tarun Mishra, Prince Harshwardhan
 
-#ifndef CRED_CMS_H
-#define CRED_CMS_H
+#ifndef CMS_H
+#define CMS_H
 
-#endif //CRED_CMS_H
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Contact {
+   public:
+    Contact(const string &firstName, const string &lastName,
+            const string &phoneNumber);
+
+    const string getFirstName();
+    const string getLastName();
+    const string getPhoneNumber();
+
+   private:
+    string firstName;
+    string lastName;
+    string phoneNumber;
+};
+
+class ContactManager {
+   public:
+    void addContact(const Contact &contact);
+
+    vector<Contact> searchByFirstName(const string &firstName);
+    vector<Contact> searchByLastName(const string &lastName);
+    vector<Contact> searchByPhoneNumber(const string &phoneNumber);
+
+   private:
+    vector<Contact> contacts;
+};
+
+#endif  // CMS_H
